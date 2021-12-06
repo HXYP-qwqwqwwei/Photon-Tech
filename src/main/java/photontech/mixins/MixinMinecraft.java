11690dbfbebe5825e0abc2a4b91d6688749f4a2c
HXYP-qwqwqwwei <qwqwqwwei@qq.com>
2021-12-07 00:03:42 +0800

    Mixin配置和基本测试

--- src/main/java/photontech/mixins/MixinMinecraft.java
+++ src/main/java/photontech/mixins/MixinMinecraft.java
@@ -1,18 +1,19 @@
 package photontech.mixins;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.network.play.ClientPlayNetHandler;
 import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft {
-    /**
-     * @author HXYP
-     * @reason For Test
-     */
-    @Overwrite
-    private String createTitle() {
-        return "PhotonTech";
+
+    @Inject(method = "createTitle", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
+    private void onCreateTitle(CallbackInfoReturnable<String> cir, StringBuilder stringbuilder, ClientPlayNetHandler clientplaynethandler) {
+        cir.setReturnValue(stringbuilder.append("-PtTest").toString());
     }
 }
